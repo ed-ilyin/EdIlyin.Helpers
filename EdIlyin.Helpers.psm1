815a3c49fd703601bb8487369f0e923228a4e591
Ed Ilyin <ed.il@live.com>
2016-05-03 15:46:53 +0300

    initial commit

---
+++ EdIlyin.Helpers.psm1
@@ -0,0 +1,23 @@
+function Get-EihHelp {
+    param (
+        [Parameter(ValueFromPipeline)][string]$Name
+    )
+    $Command = $Name | gcm
+    if ($Command.ResolvedCommand) {
+        $Alias = $Command.DisplayName
+        $Command = $Command.ResolvedCommand
+    }
+    else {
+        $Alias = "$(gal -def $Name)"
+    }
+    [pscustomobject]@{
+        Alias = $Alias
+        ParameterSets = $Command.ParameterSets
+    } | Write-Output
+}
+function Show-EihHelp {
+    param (
+        [Parameter(ValueFromPipeline)][string]$Name
+    )
+    $Name | Get-EihHelp | Show-Object -d 5
+}
